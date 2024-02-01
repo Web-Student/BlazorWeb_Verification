f0bb736c24f512680140a6bdea336b5992b25c8a
rachel.allen1 <rachel.allen1@ENGR2JCP5S3>
2024-02-01 13:49:36 -0700

    Have a valid site key, now it comoplains about not having a valid antiforgery token. Also I need to do the servier side integration

--- BlazorWeb_Verification/Program.cs
+++ BlazorWeb_Verification/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+//var RecaptchaKey = builder.Configuration["Google:siteKey"];
 
 var app = builder.Build();
 
